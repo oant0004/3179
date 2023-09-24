d1687219b61256908410d91c6170e0f4549f5da1
oant0004 <oant0004@student.monash.edu>
2023-09-24 20:36:53 +1000

    initial

---
+++ vega_lite_vis.js
@@ -0,0 +1,9 @@
+var vg_1 = "js/heatmap_vg.json";
+vegaEmbed("#heat_map", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2= "globalmap_vg.json";
+vegaEmbed("#global_map", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
