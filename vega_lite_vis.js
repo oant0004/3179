d9749ee5edf6c1414edb87b62920ed63f4887581
oant0004 <oant0004@student.monash.edu>
2023-09-25 16:11:31 +1000

    .

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -7,3 +7,8 @@ var vg_2 = "globalmap_vg.json";
 vegaEmbed("#global_map", vg_2).then(function(result) {
     // Access the Vega view instance as result.view
 }).catch(console.error);
+
+var vg_dotplot = "dotplot_vg.json";
+vegaEmbed("#dot_plot", vg_dotplot).then(function(result) {
+    // Access the Vega view instance as result.view
+}).catch(console.error);
